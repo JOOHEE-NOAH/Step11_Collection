27e06261dc62227670f897769a96188067858e8e
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-10 12:17:03 +0900

    initial commit

---
+++ src/collection1/ListEx2.java
@@ -0,0 +1,22 @@
+package collection1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//제네릭스(generics)
+public class ListEx2 {
+
+	public static void main(String[] args) {
+		List<Object> list=new ArrayList<>(); //<String>:받아쓰는애는 String자료형이어야한다는 의미
+		
+		list.add("봄");
+		list.add("여름");
+		list.add("가을");
+		list.add(Integer.valueOf(10));//  근데 그러면 이건 자료형이 안맞아서 에러뜸 따라서 위의 <String>을 <Object>로 바꿔주면 아무거나 사용가능
+		list.add(4.0f);
+		list.add(10.0);
+		
+		System.out.println(list);
+	}
+
+}
